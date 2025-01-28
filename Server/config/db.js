2192eecb2110e189bc063a586bea15e5859abeb2
dev-code-create <ayushkankale04@gmail.com>
2025-01-28 23:50:57 +0530

    User Model Done

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const mongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected");
   } catch (error) {
     console.log(error.message);
---
+++ Server/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  phone: {
+    type: String,
+    required: [true, "Phone number is required for sms reminder"],
+    unique: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
