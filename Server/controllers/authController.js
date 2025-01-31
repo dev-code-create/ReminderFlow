907975868ce10daece56433afbcd3d69ac3ba382
dev-code-create <ayushkankale04@gmail.com>
2025-01-31 23:35:19 +0530

    login controlller logic done

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -17,3 +17,22 @@ export const signup = async (req, res) => {
     res.status(500).json({ error: "Server error:" + error.message });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: "Invalid credentials!" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: "Server error: " + err.message });
+  }
+};
