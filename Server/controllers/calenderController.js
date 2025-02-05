ef8e2b94fd8673a68e0c8c5039d1caef0bbbbc58
dev-code-create <ayushkankale04@gmail.com>
2025-02-05 23:28:41 +0530

    getCalenderIntegrations functionality done

--- Server/controllers/calenderController.js
+++ Server/controllers/calenderController.js
@@ -23,3 +23,18 @@ export const connectCalender = async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
+
+export const getCalenderIntegration = async (req, res) => {
+  try {
+    const calenderIntegration = await CalenderIntegration.findOne({
+      user: req.user.id,
+    }).populate("user");
+    if (!calenderIntegration)
+      return res
+        .status(404)
+        .json({ message: "No calender intergration found" });
+    res.json(calenderIntegration);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
