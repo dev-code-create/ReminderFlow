f610bd13f0a215961729bcbfff2df11c94978313
dev-code-create <ayushkankale04@gmail.com>
2025-02-05 14:04:04 +0530

    Auth Routes defined

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,15 +1,9 @@
-// routes/authRoutes.js
 import express from "express";
-import { signup, login, updateUser } from "../controllers/authController.js";
-// import { authenticateUser } from "../middleware/authMiddleware.js";
+import { loginUser, registerUser } from "../controllers/authController";
 
 const router = express.Router();
 
-// Public routes
-router.post("/signup", signup);
-router.post("/login", login);
-
-// Protected routes (require authentication)
-router.put("/update", updateUser);
+router.post("signup", registerUser);
+router.post("signin", loginUser);
 
 export default router;
