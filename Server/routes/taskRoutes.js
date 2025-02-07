4306151a172794056056b1a7e2bcf90c9ef02e3b
dev-code-create <ayushkankale04@gmail.com>
2025-02-07 15:36:16 +0530

    bug fixed notification env remian

--- Server/routes/taskRoutes.js
+++ Server/routes/taskRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { createTask, getTasks } from "../controllers/taskController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/createTask", createTask);
 router.post("/getTask", getTasks);
+router.delete("/:taskId", authMiddleware, deleteTask);
 
 export default router;
