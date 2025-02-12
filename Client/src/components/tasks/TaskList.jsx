3679e2d25975fbf738ee9806720ccf287f1a8b31
dev-code-create <ayushkankale04@gmail.com>
2025-02-12 19:53:50 +0530

    basic  tasklist done

--- Client/src/components/tasks/TaskList.jsx
+++ Client/src/components/tasks/TaskList.jsx
@@ -21,7 +21,19 @@ const TaskList = () => {
     }
   };
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>Task List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id}>
+            {task.name}
+            <button onClick={() => handleDelete(task._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TaskList;
