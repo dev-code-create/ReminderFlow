5279e9e20f5a001f76a0dfcf2566da0b2244c525
dev-code-create <ayushkankale04@gmail.com>
2025-02-19 18:33:55 +0530

    io

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
