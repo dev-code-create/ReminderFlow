987bf6f9444010ae8a1980b974b94cec5f60c161
dev-code-create <ayushkankale04@gmail.com>
2025-03-05 21:54:08 +0530

    : (

--- Server/server.js
+++ Server/server.js
@@ -18,12 +18,7 @@ dotenv.config();
 mongoDB();
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", authRoutes);
