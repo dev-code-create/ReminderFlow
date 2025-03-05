1a41bf168d7609bc64b31ef28698e50df6670681
dev-code-create <ayushkankale04@gmail.com>
2025-03-05 23:45:12 +0530

    vite api url

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { "Content-Type": "application/json" },
-  credentials: true,
+  withCredentials: true,
 });
 
 apiClient.interceptors.request.use((config) => {
